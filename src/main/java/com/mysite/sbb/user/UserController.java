00f0188a59c58db2ac8cdb212da51741f33fb15b
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-22 11:20:27 +0900

    스프링 시큐리티와 회원가입

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
                     userCreateForm.getEmail(), userCreateForm.getPassword1());
         }catch (DataIntegrityViolationException e){
             e.printStackTrace();
-            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.")
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
             return "signup_form";
         }catch (Exception e){
             e.printStackTrace();
